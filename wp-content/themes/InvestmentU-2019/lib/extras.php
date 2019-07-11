8b20c074b42cec0bfa19169ee10a53165d532713
rfegade <fegaderashmi2@gmail.com>
2019-07-11 14:24:47 -0400

    Article page updates

--- wp-content/themes/InvestmentU-2019/lib/extras.php
+++ wp-content/themes/InvestmentU-2019/lib/extras.php
@@ -28,6 +28,8 @@ add_filter('body_class', __NAMESPACE__ . '\\body_class');
  * Clean up the_excerpt()
  */
 function excerpt_more() {
-  return ' &hellip; <a href="' . get_permalink() . '">' . __('Continued', 'sage') . '</a>';
+  return ' &hellip; <a href="' . get_permalink() . '" class="readmore"><em>' . __('Read More', 'sage') . '</em></a>';
 }
 add_filter('excerpt_more', __NAMESPACE__ . '\\excerpt_more');
+
+
