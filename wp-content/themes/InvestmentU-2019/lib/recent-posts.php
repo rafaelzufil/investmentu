79ab0a588cca1f8ba03d32fb6a019f32cb9ad3c5
rfegade <fegaderashmi2@gmail.com>
2019-07-17 11:16:49 -0400

    fix view more link

--- wp-content/themes/InvestmentU-2019/lib/recent-posts.php
+++ wp-content/themes/InvestmentU-2019/lib/recent-posts.php
@@ -48,7 +48,7 @@ function IU_recent_posts_shortcode($atts, $content = null) {
 	
 	wp_reset_postdata();
 	
-	return '<div class="row my-2 row-eq-height category-article-preview-row">'. $output .'</div><a class="btn btn-block btn-primary form-control mx-auto" href=" ' . esc_url(home_url('/')) . 'category/dividend-stocks/" style="width:60%;">View More</a>';
+	return '<div class="row my-2 row-eq-height category-article-preview-row">'. $output .'</div><a class="btn btn-block btn-primary form-control mx-auto" href=" ' . esc_url(home_url('/')) . 'category/'.  $category[0]->slug .'/" style="width:60%;">View More</a>';
 	
 }
 add_shortcode('recent_posts', 'IU_recent_posts_shortcode');
