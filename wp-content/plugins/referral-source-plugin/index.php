152d21adc3a188a81894daee61b19e9f00b88cfd
jhill3rd <jhill@oxfordclub.com>
2019-07-17 13:10:13 -0400

    Rename theme; Add revive scripts; Update referral source plugin

--- wp-content/plugins/referral-source-plugin/index.php
+++ wp-content/plugins/referral-source-plugin/index.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
-  Plugin Name: Referral Source Plugin
+  Plugin Name: Referral referral_source Plugin
   Description: Check referer path and set the approriate cookie
   Plugin URI: //oxfordclub.com
   Author: Joseph Hill
@@ -9,44 +9,47 @@
   Version: 2.0
  */
 
-function get_traffic_source() {
+function set_referral_cookie() {
 
-  $referer = $_SERVER['HTTP_REFERER'];
-  $engines = ['google','yahoo','aol','msn','ask','altavista','snap','alltheweb','gigablast','hotbot','netscape','live','exalead','bing','cnn','msnbc','lycos','duckduckgo'];
-  $social = ['facebook','twitter','plus.google.com','linkedin','youtube','instagram','pinterest'];
+  if (!isset($_COOKIE['referral_source'])) {
 
-
-  if (!isset($_COOKIE['source'])) {
+    $referer = $_SERVER['HTTP_REFERER'];
+    $engines = ['google','yahoo','aol','msn','ask','altavista','snap','alltheweb','gigablast','hotbot','netscape','live','exalead','bing','cnn','msnbc','lycos','duckduckgo'];
+    $social = ['facebook','twitter','plus.google.com','linkedin','youtube','instagram','pinterest'];
+    $paid = '';
 
     if (isset($referer)) {
 
       if (str_replace($engines, '', $referer) !== $referer) {
 
-        setcookie('source', 'organic', time() + (86400 * 30), "/");
+        setcookie('referral_source', 'organic', time() + (86400 * 30), "/");
 
       } elseif (str_replace($social, '', $referer) !== $referer) {
 
-        setcookie('source', 'social', time() + (86400 * 30), "/");
+        setcookie('referral_source', 'social', time() + (86400 * 30), "/");
 
       }
 
       else {
 
-        setcookie('source', 'referral', time() + (86400 * 30), "/");
+        setcookie('referral_source', 'referral', time() + (86400 * 30), "/");
 
       }
 
     } elseif (isset($_GET['src']) && $_GET['src'] === 'email') {
 
-      setcookie('source', 'email', time() + (86400 * 30), "/");
+      setcookie('referral_source', 'email', time() + (86400 * 30), "/");
 
     } else {
 
-      setcookie('source', 'direct', time() + (86400 * 30), "/");
+      setcookie('referral_source', 'direct', time() + (86400 * 30), "/");
 
     }
+
+    echo "<script> var referral_source = '".$referral_source."';</script>";
+
   }
 
 }
 
-add_action('wp_head', 'get_traffic_source');
+add_action('init', 'set_referral_cookie');
---
+++ wp-content/plugins/revive-variables/revive-variables.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+  Plugin Name: Revive Variable Plugin
+  Description: Pass Data back to Revive for ad serving.
+  Plugin URI: //github.com/PaidSites.com
+  Author: Joseph Hill
+  Author URI: //github.com/PaidSites.com
+  Version: 1.0
+ */
+
+ function enqueue_js() {
+   wp_enqueue_script( 'revive-js', plugins_url() . '/' . basename(__DIR__)  . '/js/revive.js', array( 'jquery' ), $current_datetime, true );
+ }
+ add_action('wp_enqueue_scripts', 'enqueue_js');
