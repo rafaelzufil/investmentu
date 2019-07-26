40530777f4272028a41c51ef3dc581101c9e57af
rfegade <fegaderashmi2@gmail.com>
2019-07-26 11:47:43 -0400

    update image size

--- wp-content/themes/InvestmentU-2019/templates/content-search.php
+++ wp-content/themes/InvestmentU-2019/templates/content-search.php
@@ -3,11 +3,11 @@
     <?php //the_excerpt(); ?>
     <div class="articlePreview container">
         <div class="row row-eq-height">
-            <div class="col-5">
+            <div class="col-4">
                 <?php $date = get_the_date(); ?>
                 <a href="<?php the_permalink(); ?>"><img src="<?php the_post_thumbnail_url(); ?>" class="img-fluid" alt="..." ></a>
             </div>
-            <div class="col-7 ">
+            <div class="col-8 ">
                 <a href="<?php the_permalink(); ?>"><h4><?php the_title(); ?></h4></a>
                 <p class="articleDate publication-source"><?php echo $date; ?></p>
                 <div class="article_preview d-none d-sm-block"><?php the_excerpt(); ?></div>
