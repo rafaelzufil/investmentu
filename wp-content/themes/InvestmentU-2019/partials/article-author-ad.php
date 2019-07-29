0112f9cfd0f84c3fe1751cf16dac0d138053c073
rfegade <fegaderashmi2@gmail.com>
2019-07-29 16:45:01 -0400

    fix ad language

--- wp-content/themes/InvestmentU-2019/partials/article-author-ad.php
+++ wp-content/themes/InvestmentU-2019/partials/article-author-ad.php
@@ -5,13 +5,17 @@ if ( ! empty ( $authorDescription ) ) {
     <p><?php echo get_the_author_meta('description'); ?></p>
     <h6>Like what you’re reading from <?= get_the_author(); ?>? Sign up for his eletter, <?php echo get_post_meta( $post->ID, 'publication-source', true); ?>.</h6>
 <?php } else { ?>
-    <h6>Like what you’re reading on Investment U? Sign up for this e-letter</h6>
+    <h6>Like what you’re reading on Investment U? Sign up for our free e-letter</h6>
 <?php } ?>
 <div class="native-leadgen-signup">
-    <form>
+    <form action="https://signupapp2.com/signupapp/signups/process" method="post" name="SimpleSignUp" data-toggle="validator" role="form" id="lead-gen" "="" >
     <div class="form-group row mx-auto my-3">
         <input type="text" class="form-control col-8">
         <button class="btn btn-block btn-primary form-control col-4" type="submit">Subscribe Now</button>
+        <input name="signup.sourceId" type="hidden" value="X300V727">
+          <input name="signup.listCode" type="hidden" value="INVESTME">
+          <input name="signup.redirectUrl" id="redirectUrl" type="hidden" value="">
+          <input name="signup.welcomeEmailTemplateName" type="hidden" value="">
     </div>
     </form>
 </div>
