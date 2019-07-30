ba90a8f143813578bf74e77ee3c211a84b7f9c4e
jhill3rd <jhill@oxfordclub.com>
2019-07-30 17:54:49 -0400

    Finalize revive function for appropriate zones

--- wp-content/themes/InvestmentU-2019/partials/sidebar.php
+++ wp-content/themes/InvestmentU-2019/partials/sidebar.php
@@ -4,42 +4,7 @@
         <?php dynamic_sidebar('sidebar-primary'); ?>
         <?php
 
-          $terms = json_decode(json_encode(get_the_tags()), true);
-
-          $i = 0;
-
-          foreach ($terms as $item) {
-            if ($item['slug'] === 'wealthy-retirement') {
-              $zone = 11;
-              break;
-            }
-            if ($item['slug'] === 'liberty-through-wealth') {
-              $zone = 12;
-              break;
-            }
-            if ($item['slug'] === 'early-investing') {
-              $zone = 13;
-              break;
-            }
-            if ($item['slug'] === 'manward-digest') {
-              $zone = 14;
-              break;
-            }
-            if ($item['slug'] === 'trade-of-the-day') {
-              $zone = 15;
-              break;
-            }
-            if ($item['slug'] === 'profit-trends') {
-              $zone = 16;
-              break;
-            }
-            $i++;
-          };
-
-          if (!isset($zone)) {
-            $zone = 18;
-          }
-
+          $zone = revive_zone('sidebar');
           revive_display( $zone );
 
          ?>
