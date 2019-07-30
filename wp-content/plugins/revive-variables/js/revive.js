ec42939d3eeeca7c51db843deaeadfe8c6e54876
jhill3rd <jhill@oxfordclub.com>
2019-07-30 13:04:17 -0400

    Finalize revive function

--- wp-content/plugins/revive-variables/js/revive.js
+++ wp-content/plugins/revive-variables/js/revive.js
@@ -11,37 +11,6 @@ var revive = {
     } else var expires = "";
     document.cookie = name + "=" + value + expires + "; path=/";
   },
-  display: function(zone_id) {
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = revive.generateCode(zone_id);
-    document.getElementById( 'revive-' + zone_id).appendChild(script);
-  },
-  generateCode: function(zone_id) {
-    //var m3_u = (location.protocol == 'https:' ? 'https://ads.web.oxfordclub.com/www/delivery/ajs.php' : '//ads.web.oxfordclub.com/www/delivery/ajs.php');
-    var m3_u = 'https://ads.web.oxfordclub.com/www/delivery/ajs.php';
-    var m3_r = Math.floor(Math.random() * 99999999999);
-    var html = '';
-    if (!document.MAX_used) document.MAX_used = ',';
-    //html += "<scr" + "ipt type='text/javascript' src='" + m3_u;
-    html += m3_u;
-    html += "?zoneid=" + zone_id + "&amp;target=_blank&amp;block=1";
-    if (typeof author != 'undefined') html += "&amp;author=" + author;
-    if (typeof category != 'undefined') html += "&amp;cat=" + category;
-    if (typeof tag != 'undefined') html += "&amp;tag=" + tag;
-    html += "&amp;signup=" + revive.getCookie('signup');
-    html += '&amp;referral_source=' + revive.getCookie("referral_source");
-    if (typeof pubcodes != 'undefined' && pubcodes != "") html += "&amp;pubcodes=" + pubcodes;
-    html += '&amp;cb=' + m3_r;
-    if (document.MAX_used != ',') html += "&amp;exclude=" + document.MAX_used;
-    html += (document.charset ? '&amp;charset=' + document.charset : (document.characterSet ? '&amp;charset=' + document.characterSet : ''));
-    html += "&amp;loc=" + escape(window.location);
-    if (document.referrer) html += "&amp;referer=" + escape(document.referrer);
-    if (document.context) html += "&context=" + escape(document.context);
-    if (document.mmm_fo) html += "&amp;mmm_fo=1";
-    //html += "'><\/scr" + "ipt>";
-    return html;
-  },
   hasSignedUp: function() {
     return revive.getCookie('signup') !== null;
   }
--- wp-content/plugins/revive-variables/revive-variables.php
+++ wp-content/plugins/revive-variables/revive-variables.php
@@ -10,6 +10,60 @@
  */
 
  function enqueue_js() {
-   wp_enqueue_script( 'revive-js', plugins_url() . '/' . basename(__DIR__)  . '/js/revive.js', array( 'jquery' ), $current_datetime, false );
+   wp_enqueue_script( 'revive-js', plugins_url() . '/' . basename(__DIR__)  . '/js/revive.js', array( 'jquery' ), null, false );
  }
  add_action('wp_enqueue_scripts', 'enqueue_js');
+
+function revive_display( $zone ) {
+
+  $post_id = get_the_ID();
+  $author_id = get_post_field( 'post_author', $post_id );
+  $post_author = get_the_author_meta( 'display_name', $author_id );
+  $source = isset($_COOKIE['referral_source']) ? $_COOKIE['referral_source'] : null;
+  if (get_the_category()  !== null) {
+    $all_categories = '';
+    $categories = json_decode(json_encode(get_the_category()), true);
+    $i = 0;
+    foreach ($categories as $item) {
+      if ($i > 0) {
+        $all_categories .= ',';
+      }
+      $all_categories .= $item['slug'];
+      $i++;
+    }
+  } else {
+    $all_categories = null;
+  }
+  if (get_the_tags()  !== null) {
+    $all_tags = '';
+    $categories = json_decode(json_encode(get_the_tags()), true);
+    $i = 0;
+    foreach ($categories as $item) {
+      if ($i > 0) {
+        $all_tags .= ',';
+      }
+      $all_tags .= $item['slug'];
+      $i++;
+    }
+  } else {
+    $all_tags = null;
+  }
+  $signup = isset($_COOKIE['signup']) ? $_COOKIE['signup'] : null;
+
+  echo "
+  <!-- Revive Adserver Asynchronous JS Tag - Generated with Revive Adserver v4.2.1 -->
+  <ins
+    data-revive-zoneid=\"$zone\"
+    data-revive-block=\"1\"
+    data-revive-blockcampaign=\"1\"
+    data-revive-id=\"e381927fbe56d09bfb1b715794e5c109\"
+    data-revive-category=\"$all_categories\"
+    data-revive-tags=\"$all_tags\"
+    data-revive-author=\"$post_author\"
+    data-revive-signup=\"$signup\"
+    data-revive-referral_source=\"$source\"
+  ></ins>
+  <script async src=\"https://ads.web.oxfordclub.com/www/delivery/asyncjs.php\"></script>";
+
+
+}
