f005b925e5e2f2d3925392c1c721d4498ea7b545
rfegade <fegaderashmi2@gmail.com>
2019-07-30 19:14:47 -0400

    fix top posts

--- wp-content/themes/InvestmentU-2019/functions.php
+++ wp-content/themes/InvestmentU-2019/functions.php
@@ -108,38 +108,38 @@ function create_post_type() {
 add_action( 'init', 'create_post_type' );
 
 
-/* Tmestamp for Homepage Articles */
+// /* Tmestamp for Homepage Articles */
 
-    define( TIMEBEFORE_NOW,         'now' );
-    define( TIMEBEFORE_MINUTE,      '{num} minute ago' );
-    define( TIMEBEFORE_MINUTES,     '{num} minutes ago' );
-    define( TIMEBEFORE_HOUR,        '{num} hour ago' );
-    define( TIMEBEFORE_HOURS,       '{num} hours ago' );
-    define( TIMEBEFORE_YESTERDAY,   'yesterday' );
-    define( TIMEBEFORE_FORMAT,      '%e %b' );
-    define( TIMEBEFORE_FORMAT_YEAR, '%e %b, %Y' );
+//     define( TIMEBEFORE_NOW,         'now' );
+//     define( TIMEBEFORE_MINUTE,      '{num} minute ago' );
+//     define( TIMEBEFORE_MINUTES,     '{num} minutes ago' );
+//     define( TIMEBEFORE_HOUR,        '{num} hour ago' );
+//     define( TIMEBEFORE_HOURS,       '{num} hours ago' );
+//     define( TIMEBEFORE_YESTERDAY,   'yesterday' );
+//     define( TIMEBEFORE_FORMAT,      '%e %b' );
+//     define( TIMEBEFORE_FORMAT_YEAR, '%e %b, %Y' );
 
-    function time_ago( $time )
-    {
-        $out    = ''; // what we will print out
-        $now    = time(); // current time
-        $diff   = $now - $time; // difference between the current and the provided dates
+//     function time_ago( $time )
+//     {
+//         $out    = ''; // what we will print out
+//         $now    = time(); // current time
+//         $diff   = $now - $time; // difference between the current and the provided dates
 
-        if( $diff < 60 ) // it happened now
-            return TIMEBEFORE_NOW;
+//         if( $diff < 60 ) // it happened now
+//             return TIMEBEFORE_NOW;
 
-        elseif( $diff < 3600 ) // it happened X minutes ago
-            return str_replace( '{num}', ( $out = round( $diff / 60 ) ), $out == 1 ? TIMEBEFORE_MINUTE : TIMEBEFORE_MINUTES );
+//         elseif( $diff < 3600 ) // it happened X minutes ago
+//             return str_replace( '{num}', ( $out = round( $diff / 60 ) ), $out == 1 ? TIMEBEFORE_MINUTE : TIMEBEFORE_MINUTES );
 
-        elseif( $diff < 3600 * 24 ) // it happened X hours ago
-            return str_replace( '{num}', ( $out = round( $diff / 3600 ) ), $out == 1 ? TIMEBEFORE_HOUR : TIMEBEFORE_HOURS );
+//         elseif( $diff < 3600 * 24 ) // it happened X hours ago
+//             return str_replace( '{num}', ( $out = round( $diff / 3600 ) ), $out == 1 ? TIMEBEFORE_HOUR : TIMEBEFORE_HOURS );
 
-        elseif( $diff < 3600 * 24 * 2 ) // it happened yesterday
-            return TIMEBEFORE_YESTERDAY;
+//         elseif( $diff < 3600 * 24 * 2 ) // it happened yesterday
+//             return TIMEBEFORE_YESTERDAY;
 
-        else // falling back on a usual date format as it happened later than yesterday
-            return strftime( date( 'Y', $time ) == date( 'Y' ) ? TIMEBEFORE_FORMAT : TIMEBEFORE_FORMAT_YEAR, $time );
-    }
+//         else // falling back on a usual date format as it happened later than yesterday
+//             return strftime( date( 'Y', $time ) == date( 'Y' ) ? TIMEBEFORE_FORMAT : TIMEBEFORE_FORMAT_YEAR, $time );
+//     }
 
   // Add img-fluid class to all images
   function add_image_responsive_class($content) {
