d813649f7cba899ce48ff3445ff4429d2f38276d
jhill3rd <jhill@oxfordclub.com>
2019-08-06 19:15:57 -0400

    Update logic

--- wp-content/themes/InvestmentU-2019/assets/scripts/email-validation.js
+++ wp-content/themes/InvestmentU-2019/assets/scripts/email-validation.js
@@ -7,7 +7,34 @@ $(document).on('submit', '#lead-gen', function(e) {
   var welcomeEmail = $(this).closest("form").find("input[name='signup.welcomeEmailTemplateName']").val();
 
   e.preventDefault();
-  
+
+  $.ajax({
+      type        : 'GET', // define the type of HTTP verb we want to use (POST for our form)
+      url         : '/apps/signupapp.php?email='+emailAddress+'&source='+sourceId+'&list='+listCode+'&welcome='+welcomeEmail, // the url where we want to POST
+      //data        : formData, // our data object
+      dataType    : 'json', // what type of data do we expect back from the server
+      encode          : true
+  }) // using the done promise callback
+  .done(function(data) {
+
+      // log data to the console so we can see
+      if (data === 'success') {
+
+        displayConfirmModal(listCode);
+        revive.setCookie(listCode, true, 365);
+
+      } else if (data === 'duplicate') {
+
+      } else {
+
+      }
+
+      // here we will handle errors and validation messages
+  });
 
 
 });
+
+function displayConfirmModal( list ) {
+  alert (list);
+};
