ed73e4930357d47304d57fe877643be8e07b3359
Cory Blische <cblische@oxfordclub.com>
2019-09-04 11:02:53 -0400

    removed unused themes

--- wp-content/themes/investment-u/templates/footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="content-info">
-  <div class="container">
-    &copy; <?php echo date('Y'); ?> Monument Traders Alliance, LLC
-    <?php dynamic_sidebar('sidebar-footer'); ?>
-  </div>
-</footer>
