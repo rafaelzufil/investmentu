b492a4efe7f6dcb928db0e826a68ad25bf251155
balazsbohonyi <balazs.bohonyi@gmail.com>
2019-10-29 20:46:04 +0200

    Added a JS library to add support for WebP images in browsers not supporting it

--- wp-content/themes/InvestmentU-2019/base.php
+++ wp-content/themes/InvestmentU-2019/base.php
@@ -47,5 +47,13 @@ use Roots\Sage\Wrapper;
       get_template_part('templates/footer');
       wp_footer();
     ?>
+
+    <!-- WebP Image support for browsers not supporting it -->
+    <script>
+        (function(){var WebP=new Image();WebP.onload=WebP.onerror=function(){
+            if(WebP.height!=2){var sc=document.createElement('script');sc.type='text/javascript';sc.async=true;
+                var s=document.getElementsByTagName('script')[0];sc.src='<?php echo get_template_directory_uri() ?>/assets/scripts/webpjs-0.0.2.min.js';s.parentNode.insertBefore(sc,s);}};
+            WebP.src='data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';})();
+    </script>
   </body>
 </html>
