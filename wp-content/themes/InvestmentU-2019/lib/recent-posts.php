66b38a8137d33de39b13d58c2460c03958e0349f
balazsbohonyi <balazs.bohonyi@gmail.com>
2019-10-29 16:06:18 +0200

    Small change to load properly sized post thumbs for recent posts

--- wp-content/themes/InvestmentU-2019/lib/recent-posts.php
+++ wp-content/themes/InvestmentU-2019/lib/recent-posts.php
@@ -26,8 +26,7 @@ function IU_recent_posts_shortcode($atts, $content = null) {
 	foreach($posts as $post) {
 
     if ( has_post_thumbnail() ) {
-//        $img_url = get_the_post_thumbnail_url($post, 'related-posts-thumbnail');
-        $img_url = wp_get_attachment_url($post->ID, 'related-posts-thumbnail' );
+        $img_url = get_the_post_thumbnail_url($post, 'related-posts-thumbnail');
     } else {
         $img_url = 'https://s3.amazonaws.com/assets.investmentu.com/images/iu-default-image.jpg';
     }
