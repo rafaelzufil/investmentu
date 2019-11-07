69de90814e3c6fbcff1bf576920abde555b19bf1
rfegade <fegaderashmi2@gmail.com>
2019-11-07 10:55:24 -0500

    Update avatar function call (use ACF), remove unnecessory plugins

--- wp-content/themes/InvestmentU-2019/templates/entry-meta.php
+++ wp-content/themes/InvestmentU-2019/templates/entry-meta.php
@@ -1,8 +1,11 @@
 <div class="byline-container py-2">
-    <?php $headshot_url = get_wp_user_avatar_src(); ?>
-    
+    <?php 
+        $auth_id = get_the_author_meta('ID'); 
+        $headshot_url = get_field('author_headshot', 'user_' . $auth_id); 
+    ?>
+   
     <?php if(strpos($headshot_url, "blank") == false) {?>
-    <img src="<?php echo get_wp_user_avatar_src(); ?>" class="headshot article-headshot align-middle d-inline-block">
+    <img src="<?php the_field('author_headshot', 'user_' . $auth_id); ?>" class="headshot article-headshot align-middle d-inline-block">
     <?php } ?>
     <div class="align-middle d-inline-block ml-1">
         <p class="byline m-0"><?= __('By', 'sage'); ?> <a href="<?= get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author" class="fn"><?= get_the_author(); ?></a></p>        
