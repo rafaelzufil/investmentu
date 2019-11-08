290b1049336933bf6aaadf35723b88a82f1d0301
Rafael <rafael.l@onerhino.com>
2019-11-08 18:47:11 -0300

    #17067 Fixing Wistia components

--- wp-content/themes/InvestmentU-2019/single-video.php
+++ wp-content/themes/InvestmentU-2019/single-video.php
@@ -21,14 +21,21 @@
                 ?>
                 <?php if(!empty($wistia)) { ?>
                     <!-- Wistia Video -->
-                    <script src="https://fast.wistia.com/embed/medias/<?php echo $wistia; ?>.jsonp" async></script>
-                    <script src="https://fast.wistia.com/assets/external/E-v1.js" async></script>
-                    <div class="wistia_responsive_padding" style="position:relative;">
-                    <div class="wistia_responsive_wrapper" style="height:100%;left:0;top:0;width:100%;">
-                    <div class="wistia_embed wistia_async_<?php echo $wistia; ?> seo=false videoFoam=true" style="height:100%;position:relative;width:100%">
-                    <div class="wistia_swatch" style="height:100%;left:0;opacity:0;overflow:hidden;position:absolute;top:0;transition:opacity 200ms;width:100%;">
-                    <img src="https://fast.wistia.com/embed/medias/<?php echo $wistia; ?>/swatch" style="filter:blur(5px);height:100%;object-fit:contain;width:100%;" alt="" onload="this.parentNode.style.opacity=1;" />
-                    </div></div></div></div>
+                    <?php if ( function_exists( 'is_amp_endpoint' ) && is_amp_endpoint() ): ?>
+                        <script async custom-element="amp-wistia-player" src="https://cdn.ampproject.org/v0/amp-wistia-player-0.1.js"></script>
+                        <amp-wistia-player
+                            data-media-hashed-id="<?php echo $wistia; ?>"
+                            width="512" height="360"></amp-wistia-player>
+                    <?php else: ?>
+                      <script src="https://fast.wistia.com/embed/medias/<?php echo $wistia; ?>.jsonp" async></script>
+                      <script src="https://fast.wistia.com/assets/external/E-v1.js" async></script>
+                      <div class="wistia_responsive_padding" style="position:relative;">
+                      <div class="wistia_responsive_wrapper" style="height:100%;left:0;top:0;width:100%;">
+                      <div class="wistia_embed wistia_async_<?php echo $wistia; ?> seo=false videoFoam=true" style="height:100%;position:relative;width:100%">
+                      <div class="wistia_swatch" style="height:100%;left:0;opacity:0;overflow:hidden;position:absolute;top:0;transition:opacity 200ms;width:100%;">
+                      <img src="https://fast.wistia.com/embed/medias/<?php echo $wistia; ?>/swatch" style="filter:blur(5px);height:100%;object-fit:contain;width:100%;" alt="" onload="this.parentNode.style.opacity=1;" />
+                      </div></div></div></div>
+                    <?php endif; ?>
                 <?php } elseif(!empty($youtube)) { ?>
                     <!-- Youtube Video -->
                     <object style="width:100%;height:420px; float: none; clear: both; margin: 0 auto;"
