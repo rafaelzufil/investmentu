a229125c5bd7a02314ff204c40be8b3cf44be6c0
balazsbohonyi <balazs.bohonyi@gmail.com>
2019-11-12 15:20:53 +0200

    Tests with JS code loaded for AMP pages

--- wp-content/themes/InvestmentU-2019/assets/scripts-amp/amp-search-form-v14.js
+++ wp-content/themes/InvestmentU-2019/assets/scripts-amp/amp-search-form-v14.js
@@ -14,23 +14,25 @@ console.log('>>>>>>>>>>>>>>>>>> amp-search-form Javascript file loaded!!! <<<<<<
 $('amp-img#logo').remove();
 
 /* Custom jQuery("like") code */
-$('.search-form-trigger').on('click', function () {
-    $('.navbar').toggleClass('mb-5');
-    $('.search-form-wrapper').toggleClass('open');
-    $('.search-form-wrapper .search').trigger('focus');
-    $('html').toggleClass('search-form-open');
-});
+$(document).ready(function() {
+    $('.search-form-trigger').on('click', function () {
+        $('.navbar').toggleClass('mb-5');
+        $('.search-form-wrapper').toggleClass('open');
+        $('.search-form-wrapper .search').trigger('focus');
+        $('html').toggleClass('search-form-open');
+    });
 
 // $('[data-toggle=search-form-close]').on('click', function () {
 //     $('.search-form-wrapper').removeClass('open');
 //     $('html').removeClass('search-form-open');
 // });
 
-$('.search-form-wrapper .search').on('keypress', function (event) {
-    if ($(this).val() == "Search") $(this).val("");
-});
+    $('.search-form-wrapper .search').on('keypress', function (event) {
+        if ($(this).val() == "Search") $(this).val("");
+    });
 
-$('.search-close').on('click', function () {
-    $('.search-form-wrapper').removeClass('open');
-    $('html').removeClass('search-form-open');
+    $('.search-close').on('click', function () {
+        $('.search-form-wrapper').removeClass('open');
+        $('html').removeClass('search-form-open');
+    });
 });
