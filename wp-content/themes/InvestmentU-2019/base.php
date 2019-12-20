adf4600878a5cda0ae8c23ff69e99f57aba217ee
TJ Tate <tj@tjtate.com>
2019-12-20 11:16:44 -0800

    removing GTM from theme

--- wp-content/themes/InvestmentU-2019/base.php
+++ wp-content/themes/InvestmentU-2019/base.php
@@ -9,12 +9,6 @@ use Roots\Sage\Wrapper;
 <html <?php language_attributes(); ?>>
   <?php get_template_part('templates/head'); ?>
   <body <?php body_class(); ?>>
-
-    <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W57MZD"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
-
     
     <!--[if IE]>
       <div class="alert alert-warning">
