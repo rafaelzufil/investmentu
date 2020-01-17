910b76da3d826bd435fb8da5204fac63564625fe
Rafael <rafael.l@onerhino.com>
2020-01-17 14:54:52 -0300

    #18017 Removing carousel from AMP version

--- wp-content/themes/InvestmentU-2019/partials/homepage/section6.php
+++ wp-content/themes/InvestmentU-2019/partials/homepage/section6.php
@@ -9,12 +9,12 @@
     $the_query = new WP_Query ( $args );
 ?>
 
-<div class="container py-4" id="section06">
-    <h1 class="mb-3 small-title">Meet the Experts</h1>
-    <div class="container text-center my-3">
-        <div class="row mx-auto my-auto">
-            <?php if (function_exists( 'is_amp_endpoint' ) && is_amp_endpoint()): ?>
-                <!-- <div id="expertCarousel" class="carousel slide w-100" data-ride="carousel">
+<?php if (function_exists( 'is_amp_endpoint' ) && is_amp_endpoint()): ?>
+    <!-- <div class="container py-4" id="section06">
+        <h1 class="mb-3 small-title">Meet the Experts</h1>
+        <div class="container text-center my-3">
+            <div class="row mx-auto my-auto">
+                <div id="expertCarousel" class="carousel slide w-100" data-ride="carousel">
                     <amp-carousel
                       layout="fixed-height"
                       type="carousel"
@@ -42,28 +42,35 @@
                             <?php endif; ?>
                         <?php endwhile; endif; ?>
                     </amp-carousel>
-                </div> -->
-            <?php else: ?>
+                </div>
+            </div>
+        </div>
+    </div> -->
+<?php else: ?>
+    <div class="container py-4" id="section06">
+        <h1 class="mb-3 small-title">Meet the Experts</h1>
+        <div class="container text-center my-3">
+            <div class="row mx-auto my-auto">
                 <div id="expertCarousel" class="carousel slide w-100" data-ride="carousel">
                     <?php if( have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
                         <div class="carousel-inner w-100" role="listbox">
                             <?php $count = 0; ?>
-                                <?php  if( have_rows('experts') ): ?>
-                                    <?php while(have_rows('experts')) : the_row(); ?>
-                                        <div class="carousel-item item <?php echo ($count == 0) ? 'active' : ''; ?>">
-                                            <div class="col-lg-2">
-                                                <div class="headshot-wrapper">
-                                                    <a href="<?php the_sub_field('expert_page_link'); ?>" title="<?php the_sub_field('expert_name'); ?>" class="thumb">
-                                                    <img class="img-fluid headshot" src="<?php the_sub_field('expert_headshot'); ?>" alt="<?php the_sub_field('expert_name'); ?>" data-ignore>
-                                                    </a>
-                                                </div>
-                                                <div class="title-wrapper"><a href="<?php the_sub_field('expert_page_link'); ?>" class="p-black"><p class="mt-2"><strong><?php the_sub_field('expert_name'); ?></strong></a></p>
-                                                <p><a href="<?php the_sub_field('expert_page_link'); ?>" class="p-black"> <?php the_sub_field('expert_title'); ?></a></p></div>
+                            <?php  if( have_rows('experts') ): ?>
+                                <?php while(have_rows('experts')) : the_row(); ?>
+                                    <div class="carousel-item item <?php echo ($count == 0) ? 'active' : ''; ?>">
+                                        <div class="col-lg-2">
+                                            <div class="headshot-wrapper">
+                                                <a href="<?php the_sub_field('expert_page_link'); ?>" title="<?php the_sub_field('expert_name'); ?>" class="thumb">
+                                                <img class="img-fluid headshot" src="<?php the_sub_field('expert_headshot'); ?>" alt="<?php the_sub_field('expert_name'); ?>" data-ignore>
+                                                </a>
                                             </div>
+                                            <div class="title-wrapper"><a href="<?php the_sub_field('expert_page_link'); ?>" class="p-black"><p class="mt-2"><strong><?php the_sub_field('expert_name'); ?></strong></a></p>
+                                            <p><a href="<?php the_sub_field('expert_page_link'); ?>" class="p-black"> <?php the_sub_field('expert_title'); ?></a></p></div>
                                         </div>
+                                    </div>
                                     <?php $count++; ?>
-                                    <?php endwhile; ?>
-                                <?php endif; ?>
+                                <?php endwhile; ?>
+                            <?php endif; ?>
                         </div>
                     <?php endwhile; endif; ?>
                     <a class="carousel-control-prev w-auto" href="#expertCarousel" role="button" data-slide="prev">
@@ -75,7 +82,7 @@
                         <span class="sr-only">Next</span>
                     </a>
                 </div>
-            <?php endif; ?>
+            </div>
         </div>
     </div>
-</div>
+<?php endif; ?>
