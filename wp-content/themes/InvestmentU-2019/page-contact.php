db2378acf59bf212dfa301012fadc37ddedd9461
Rafael <rafael.l@onerhino.com>
2020-01-27 18:03:47 -0300

    #18018 Creating AMP compliant contact form

--- wp-content/themes/InvestmentU-2019/page-contact.php
+++ wp-content/themes/InvestmentU-2019/page-contact.php
@@ -1,32 +1,32 @@
 
-    <?php while (have_posts()) : the_post(); ?>
-    <main>
-        <div class="container">
-        <div class="row my-5">
-            <div class="col-12 col-md-12">
-            <div class="row">
-                <div class="col-10 mx-auto">
-                <article>
-                    <div class="article-header mb-2">
-                    <h1 class="page-title mb-4"><?php the_title(); ?></h1>
+    <?php if ( function_exists( 'is_amp_endpoint' ) && !is_amp_endpoint() ):  ?>
+        <?php while (have_posts()) : the_post(); ?>
+        <main>
+            <div class="container">
+            <div class="row my-5">
+                <div class="col-12 col-md-12">
+                <div class="row">
+                    <div class="col-10 mx-auto">
+                    <article>
+                        <div class="article-header mb-2">
+                        <h1 class="page-title mb-4"><?php the_title(); ?></h1>
+                        </div>
+                        <?php the_content(); ?>
+                        <div class="alert alert-secondary" id="alert" style="display: none;">
+                            <h4>Thanks for reaching out.</h4>
+                            <p>We will get in touch with you shortly.</p>
+                        </div>
+                        <form class="js-contact-form contactUs margin-top" id="contact_us_form"></form>
+                    </article>
                     </div>
-                    <?php the_content(); ?>
-                    <div class="alert alert-secondary" id="alert" style="display: none;">
-                        <h4>Thanks for reaching out.</h4>
-                        <p>We will get in touch with you shortly.</p>
-                    </div>
-                    <form class="js-contact-form contactUs margin-top" id="contact_us_form"></form>
-                </article>
+                </div>
                 </div>
             </div>
             </div>
-        </div>
-        </div>
 
-    </main>
-    <?php endwhile; ?>
+        </main>
+        <?php endwhile; ?>
 
-    <?php if ( function_exists( 'is_amp_endpoint' ) && !is_amp_endpoint() ):  ?>
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 
         <script type="text/javascript" src="<?php echo get_bloginfo('template_url') ?>/jquery.agora-contact-form.js"></script>
@@ -75,4 +75,64 @@
                 message.required = true;
             });
         </script>
+    <?php else: ?>
+        <?php while (have_posts()) : the_post(); ?>
+        <main>
+            <div class="container">
+            <div class="row my-5">
+                <div class="col-12 col-md-12">
+                <div class="row">
+                    <div class="col-10 mx-auto">
+                    <article>
+                        <div class="article-header mb-2">
+                        <h1 class="page-title mb-4"><?php the_title(); ?></h1>
+                        </div>
+                        <?php the_content(); ?>
+                        <div class="alert alert-secondary" id="alert" style="display: none;">
+                            <h4>Thanks for reaching out.</h4>
+                            <p>We will get in touch with you shortly.</p>
+                        </div>
+                        <form action-xhr="<?php get_template_directory_uri() . '/amp-contact-form.php'?>" class="js-contact-form contactUs margin-top" method="post" id="contact_us_form" name="SimpleSignUp" role="form" target="_top">
+                            <input type="text" name="email" class="form-control form-control-lg" placeholder="Email" required="">
+                            <input type="text" name="first_name" class="form-control form-control-lg" placeholder="First Name and Last Name" required="">
+                            <input type="text" name="phone" class="form-control form-control-lg" placeholder="Phone Number">
+                            <select name="request_type" class="form-control form-control-lg">
+                                <option value="">Reason for Contacting</option>
+                                <option value="Email Problems">Email not Received</option>
+                                <option value="Login Problems">Login Problems</option>
+                                <option value="Customer Maintenance">Update Email Address</option>
+                                <option value="Editorial Comments">Feedback to the Editor</option>
+                                <option value="Miscellaneous">Other</option>
+                                <option value="Unsubscribe">Remove Me From Email List</option>
+                                <option value="Subscription Cancellation Request">Subscription Cancellation Request</option>
+                                <option value="Auto Renew / Maintenance Fee Cancellation Request">Auto Renew / Maintenance Fee Cancellation Request</option>
+                            </select>
+                            <textarea name="message" placeholder="Questions/Comments" class="form-control form-control-lg" rows="4" cols="50"></textarea>
+                            <input type="hidden" name="website" value="customerservice@profittrends.com">
+                            <input type="hidden" name="redirect_url" value="">
+                            <button type="submit" class="btn navy-button">Send Now</button>
+                            <div class="amp-form-message">
+                                <div submit-success>
+                                    <template type="amp-mustache">
+                                        Thanks for reaching out.
+                                        We will get in touch with you shortly.
+                                    </template>
+                                </div>
+                                <div submit-error>
+                                    <template type="amp-mustache">
+                                        Error while submitting the form.
+                                        {{result}}
+                                    </template>
+                                </div>
+                            </div>
+                        </form>
+                    </article>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+
+        </main>
+        <?php endwhile; ?>
     <?php endif; ?>
