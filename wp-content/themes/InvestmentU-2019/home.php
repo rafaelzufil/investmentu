a3fad36bd4e6c237b16a40443fa8c724f04d60b5
jhill3rd <jhill@oxfordclub.com>
2020-01-28 10:29:59 -0500

    Disable spotlight ad for now

--- wp-content/themes/InvestmentU-2019/home.php
+++ wp-content/themes/InvestmentU-2019/home.php
@@ -2,7 +2,7 @@
  <main>
     <?php get_template_part('partials/homepage/section1'); ?> <!-- top article previews section -->
     <?php get_template_part('partials/homepage/section2'); ?> <!-- ?? grey banner section -->
-    <?php get_template_part('partials/homepage/spotlight'); ?> <!-- homepage spotlight -->
+    <?php //get_template_part('partials/homepage/spotlight'); ?> <!-- homepage spotlight -->
     <?php get_template_part('partials/homepage/section3'); ?> <!-- lower article previews section -->
     <?php get_template_part('partials/homepage/section4'); ?> <!-- video section -->
     <?php get_template_part('partials/homepage/section5'); ?> <!-- other links section -->
